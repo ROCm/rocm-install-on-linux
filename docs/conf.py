f094e25ba9fb13f32929f9c6e1438b7d72c33cba
Saad Rahim (AMD) <44449863+saadrahim@users.noreply.github.com>
2023-12-15 16:05:32 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ import os
 from rocm_docs import ROCmDocs
 
 # ROCm version numbers
-rocm_version = '6.0.0'
+rocm_version = '6.0'
 amdgpu_version = '6.0' # directory in # directory in https://repo.radeon.com/rocm/apt/https://repo.radeon.com/amdgpu-install/
 amdgpu_install_version = '6.0.60000-1' # version in https://repo.radeon.com/amdgpu-install/5.7.1/ubuntu/focal/
 
