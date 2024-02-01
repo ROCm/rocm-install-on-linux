04c6e4155cbdbe893f3766bff738956dac927e61
Saad Rahim (AMD) <44449863+saadrahim@users.noreply.github.com>
2024-02-01 09:57:41 -0700

    Update conf.py for 6.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 rocm_version = '6.0.2'
 rocm_directory_version = '6.0.2' # in 6.0 rcom was located in /opt/rocm-6.0.0
 amdgpu_version = '6.0.2' # directory in https://repo.radeon.com/rocm/apt/ and https://repo.radeon.com/amdgpu-install/
-amdgpu_install_version = '6.0.60000-1' # version in https://repo.radeon.com/amdgpu-install/5.7.1/ubuntu/focal/
+amdgpu_install_version = '6.0.60002-1' # version in https://repo.radeon.com/amdgpu-install/5.7.1/ubuntu/focal/
 
 latex_engine = "xelatex"
 latex_elements = {
